3b49e650f11aede731ec45c14b059b93fb9d8ddb
Adel Sevastianov <102406080+Saracomethstein@users.noreply.github.com>
2024-11-05 14:52:38 +0300

    add: test data (json and xml), basic dbreader

--- cmd/go_day_01/main.go
+++ cmd/go_day_01/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	dbreader "github.com/Saracomethstein/go_day_01/internal/DBReader"
+)
 
 func main() {
-	fmt.Println("hi!")
+	var reader dbreader.DBReader
+	filename := "recipes.json" // Укажите путь к файлу
+	format := "json"           // Выберите формат вывода
+
+	// Выберите JSON или XML reader
+	if format == "json" {
+		reader = dbreader.JSONReader{}
+	} else {
+		reader = dbreader.XMLReader{}
+	}
+
+	recipes, err := reader.Read(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	if err := dbreader.PrintRecipes(recipes, format); err != nil {
+		fmt.Println("Error printing recipes:", err)
+	}
 }
--- internal/DBReader/reader.go
+++ internal/DBReader/reader.go
@@ -1,32 +1,65 @@
 package dbreader
 
-type Resipe struct {
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+type Recipe struct {
 	Name        string   `json:"name" xml:"name"`
 	Ingredients []string `json:"ingredients" xml:"ingredients>ingredient"`
 	Time        int      `json:"stovetime" xml:"time"`
 }
 
 type DBReader interface {
-	Read(filename string) ([]Resipe, error)
-	Print(resipes []Resipe) error
+	Read(filename string) ([]Recipe, error)
 }
 
 type JSONReader struct{}
 
 type XMLReader struct{}
 
-func (r JSONReader) Read(filename string) ([]Resipe, error) {
-	return nil, nil
+func (r JSONReader) Read(filename string) ([]Recipe, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var recipes []Recipe
+	err = json.NewDecoder(file).Decode(&recipes)
+
+	return recipes, err
 }
 
-func (r XMLReader) Read(filename string) ([]Resipe, error) {
-	return nil, nil
+func (r XMLReader) Read(filename string) ([]Recipe, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var recipes []Recipe
+	err = xml.NewDecoder(file).Decode(&recipes)
+
+	return recipes, err
 }
 
-func (r JSONReader) Print(resipes []Resipe) {
-
-}
-
-func (r XMLReader) Print(resipes []Resipe) {
-
+func PrintRecipes(recipes []Recipe, format string) error {
+	var data []byte
+	var err error
+	if format == "json" {
+		data, err = json.MarshalIndent(recipes, "", "    ")
+	} else if format == "xml" {
+		data, err = xml.MarshalIndent(recipes, "", "    ")
+	} else {
+		return fmt.Errorf("unknown format: %s", format)
+	}
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(data))
+	return nil
 }
